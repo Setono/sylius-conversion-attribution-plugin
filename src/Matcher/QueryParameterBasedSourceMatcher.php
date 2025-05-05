f63f6af365b5c300ecae897e143f5a53f564b6f6
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-05 11:14:29 +0200

    Refactor query parameter matching

--- src/Matcher/QueryParameterBasedSourceMatcher.php
+++ src/Matcher/QueryParameterBasedSourceMatcher.php
@@ -9,19 +9,21 @@ use Symfony\Component\HttpFoundation\Request;
 final class QueryParameterBasedSourceMatcher implements SourceMatcherInterface
 {
     public function __construct(
-        /**
-         * @var list<string> $parameters
-         */
-        public readonly array $parameters,
+        /** @var list<array{matches: list<string>, source: string|null, medium: string|null, campaign: string|null}> $parameters */
+        private readonly array $parameters,
     ) {
     }
 
     public function match(Request $request): ?Source
     {
         foreach ($this->parameters as $parameter) {
-            $source = $request->query->get($parameter);
-            if (is_string($source) && '' !== $source) {
-                return new Source($source);
+            foreach ($parameter['matches'] as $match) {
+                $source = $request->query->get($match);
+                if (!is_string($source) || '' === $source) {
+                    continue;
+                }
+
+                return new Source($parameter['source'] ?? $source, $parameter['medium'], $parameter['campaign']);
             }
         }
 
