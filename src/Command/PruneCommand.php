3feceeef0158ecae3476a754267da4078c4126da
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-05 14:38:51 +0200

    Make prune command more user friendly

--- src/Command/PruneCommand.php
+++ src/Command/PruneCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\SyliusConversionAttributionPlugin\Command;
 
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Setono\Doctrine\ORMTrait;
 use Setono\SyliusConversionAttributionPlugin\Model\SourceInterface;
@@ -11,6 +12,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Webmozart\Assert\Assert;
 
 #[AsCommand(
@@ -36,8 +38,20 @@ final class PruneCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        while (($deletableIds = $this->provideDeletableIds()) !== []) {
-            $this
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Pruning old conversion attribution sources');
+
+        $total = (int) $this->getDeletableQueryBuilder()->select('COUNT(o)')->getQuery()->getSingleScalarResult();
+        if ($total === 0) {
+            $io->success('No sources to prune');
+
+            return 0;
+        }
+
+        $io->progressStart($total);
+
+        while (($deletableIds = $this->getDeletableIds()) !== []) {
+            $progress = (int) $this
                 ->getRepository($this->sourceClass)
                 ->createQueryBuilder('o')
                 ->delete()
@@ -46,22 +60,33 @@ final class PruneCommand extends Command
                 ->getQuery()
                 ->execute()
             ;
+
+            $io->progressAdvance($progress);
         }
+
+        $io->progressFinish();
 
         return 0;
     }
 
-    private function provideDeletableIds(): array
+    private function getDeletableIds(): array
+    {
+        return $this
+            ->getDeletableQueryBuilder()
+            ->select('o.id')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getScalarResult()
+        ;
+    }
+
+    private function getDeletableQueryBuilder(): QueryBuilder
     {
         return $this
             ->getRepository($this->sourceClass)
             ->createQueryBuilder('o')
-            ->select('o.id')
             ->andWhere('o.createdAt < :date')
             ->setParameter('date', new \DateTimeImmutable(sprintf('-%d days', $this->daysToKeep)))
-            ->setMaxResults(500)
-            ->getQuery()
-            ->getScalarResult()
         ;
     }
 }
