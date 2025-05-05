ad913a153d95149e9df04465d19c864e464d3f55
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-05 08:53:41 +0200

    Use attributes instead of static props

--- src/Command/PruneCommand.php
+++ src/Command/PruneCommand.php
@@ -7,18 +7,19 @@ namespace Setono\SyliusConversionAttributionPlugin\Command;
 use Doctrine\Persistence\ManagerRegistry;
 use Setono\Doctrine\ORMTrait;
 use Setono\SyliusConversionAttributionPlugin\Model\SourceInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Webmozart\Assert\Assert;
 
+#[AsCommand(
+    name: 'setono:sylius-conversion-attribution:prune',
+    description: 'Prunes old conversion attribution sources',
+)]
 final class PruneCommand extends Command
 {
     use ORMTrait;
-
-    protected static $defaultName = 'setono:sylius-conversion-attribution:prune';
-
-    protected static $defaultDescription = 'Prunes old conversion attribution sources';
 
     public function __construct(
         ManagerRegistry $managerRegistry,
