7a7b9454a0c141005272fd1b211e291c07d39075
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-05 09:24:25 +0200

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,11 @@
 <?php
 
+use Setono\TagBag\Tag\InlineScriptTag;
+use Setono\TagBag\TagBagInterface;
+use Setono\TagBagBundle\SetonoTagBagBundle;
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreUnknownClasses([SetonoTagBagBundle::class, TagBagInterface::class, InlineScriptTag::class])
 ;
