8a15f469790af3ce7ed43ac9be0853e20ae13856
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-05 13:25:36 +0200

    Make the configuration better DX wise

--- src/DependencyInjection/SetonoSyliusConversionAttributionExtension.php
+++ src/DependencyInjection/SetonoSyliusConversionAttributionExtension.php
@@ -9,10 +9,11 @@ use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceE
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
-final class SetonoSyliusConversionAttributionExtension extends AbstractResourceExtension
+final class SetonoSyliusConversionAttributionExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -29,48 +30,10 @@ final class SetonoSyliusConversionAttributionExtension extends AbstractResourceE
         $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $queryParameters = array_merge_recursive([
-            'facebook' => [
-                'matches' => ['fbclid'], 'source' => 'facebook.com', 'medium' => 'cpc',
-            ],
-            'google' => [
-                'matches' => ['gclid', 'gbraid', 'wbraid'], 'source' => 'google', 'medium' => 'cpc',
-            ],
-            'microsoft' => [
-                'matches' => ['msclkid'], 'source' => 'bing', 'medium' => 'cpc',
-            ],
-            'generic' => [
-                'matches' => ['source', 'ref'],
-            ],
-            'tiktok' => [
-                'matches' => ['ttclid'], 'source' => 'tiktok', 'medium' => 'cpc',
-            ],
-            'x' => [
-                'matches' => ['twclid'], 'source' => 'x', 'medium' => 'cpc',
-            ],
-        ], $config['query_parameters']);
-        $queryParameters = array_filter($queryParameters, static function (array $queryParameter): bool {
-            return !array_key_exists('enabled', $queryParameter) || true === $queryParameter['enabled'];
-        });
-        $queryParameters = array_map(
-            /** @param array{matches: list<string>, source?: list<string|null>|string|null, medium?: list<string|null>|string|null, campaign?: list<string|null>|string|null, enabled?: bool} $queryParameter */
-            static function (array $queryParameter): array {
-                unset($queryParameter['enabled']);
-
-                $queryParameter['matches'] = array_values(array_unique($queryParameter['matches']));
-
-                // Because array_merge_recursive will merge associative keys by creating a new array, we will turn it back into a string
-                $queryParameter['source'] = self::firstValue($queryParameter['source'] ?? null);
-                $queryParameter['medium'] = self::firstValue($queryParameter['medium'] ?? null);
-                $queryParameter['campaign'] = self::firstValue($queryParameter['campaign'] ?? null);
-
-                return $queryParameter;
-            },
-            $queryParameters,
-        );
-
         $container->setParameter('setono_sylius_conversion_attribution.javascript.enabled', $config['javascript']['enabled']);
-        $container->setParameter('setono_sylius_conversion_attribution.query_parameters', $queryParameters);
+        $container->setParameter('setono_sylius_conversion_attribution.query_parameters', array_filter($config['query_parameters'], static function (array $queryParameter): bool {
+            return !array_key_exists('enabled', $queryParameter) || true === $queryParameter['enabled'];
+        }));
         $container->setParameter('setono_sylius_conversion_attribution.session_timeout', $config['session_timeout']);
         $container->setParameter('setono_sylius_conversion_attribution.referrers.cache.file', '%kernel.cache_dir%/referrers.php');
 
@@ -114,18 +77,29 @@ final class SetonoSyliusConversionAttributionExtension extends AbstractResourceE
         return isset($bundles[$bundle->getName()]);
     }
 
-    /**
-     * @param string|list<string|null>|null $value
-     */
-    private static function firstValue(null|string|array $value): ?string
+    public function prepend(ContainerBuilder $container): void
     {
-        $array = (array) $value;
-        foreach ($array as $item) {
-            if (null !== $item) {
-                return $item;
-            }
-        }
-
-        return null;
+        $container->prependExtensionConfig('setono_sylius_conversion_attribution', [
+            'query_parameters' => [
+                'facebook' => [
+                    'matches' => ['fbclid'], 'source' => 'facebook.com', 'medium' => 'cpc',
+                ],
+                'google' => [
+                    'matches' => ['gclid', 'gbraid', 'wbraid'], 'source' => 'google', 'medium' => 'cpc',
+                ],
+                'microsoft' => [
+                    'matches' => ['msclkid'], 'source' => 'bing', 'medium' => 'cpc',
+                ],
+                'generic' => [
+                    'matches' => ['source', 'ref'],
+                ],
+                'tiktok' => [
+                    'matches' => ['ttclid'], 'source' => 'tiktok', 'medium' => 'cpc',
+                ],
+                'x' => [
+                    'matches' => ['twclid'], 'source' => 'x', 'medium' => 'cpc',
+                ],
+            ],
+        ]);
     }
 }
